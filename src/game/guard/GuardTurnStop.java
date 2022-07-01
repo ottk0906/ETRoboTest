e5d45eb002ee92150dac38e6dc34fa255a48c280
おかど　たけとし <106789869+t-okado@users.noreply.github.com>
2022-07-01 12:20:00 +0900

    角度指定走行、距離指定走行の修正完了

--- src/game/guard/GuardTurnStop.java
+++ src/game/guard/GuardTurnStop.java
@@ -8,13 +8,42 @@ import body.Body;
  */
 public class GuardTurnStop extends Guard {
 
+	private double startRadian;	//計測開始時の積算回転角度（ラジアン）
+	private double moveRadian;	//目標回転角度（ラジアン）
+
     /**
-     * 旋回停止を判定する
-     * @return	True : 旋回停止 / False ： 旋回中
+     * コンストラクタ
+     * @param moveAngle	目標旋回角度(360°単位)
+     */
+	public GuardTurnStop(double moveAngle) {
+		//走行体の目標回転角度を設定する（ラジアン）
+		this.moveRadian = moveAngle * (Math.PI / 180);
+		//計測開始時の積算回転角度を初期化する
+		this.startRadian = 0.0;
+	}
+
+    /**
+     * 回転停止を判定する
+     * @return	True : 回転停止 / False ： 回転中
      */
 	@Override
 	public boolean judge() {
-		return Body.selfPos.isTurnStopped();
+		//現在の走行体の積算旋回角度を取得する
+		double tmpRadian = Body.selfPos.getAfterRadian();
+
+		//初回の判定処理開始時点から計測を開始する
+		if(startRadian == 0.0) {
+			//計測開始時の積算回転角度を設定する
+			this.startRadian = tmpRadian;
+			return false;
+		} else {
+			//「現在の積算回転角度」と「計測開始時の積算回転角度」の差分値が目標回転角度以上になった場合に回転停止と判定する
+			if (Math.abs(tmpRadian - startRadian) >= moveRadian) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 
 }
