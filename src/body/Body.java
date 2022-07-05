e64d5b5ef9c81872d14ea5c7bbaee5b150c2a855
おおた　とうき <95706374+ottk0906@users.noreply.github.com>
2022-07-05 15:00:33 +0900

    Update Body.java

--- src/body/Body.java
+++ src/body/Body.java
@@ -8,6 +8,7 @@ import body.measure.MeasureArm;
 import body.measure.MeasureCourse;
 import body.measure.MeasureTouch;
 import body.measure.MeasureWheel;
+import game.SelfPosition.SelfPosition;
 import hardware.KamogawaRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
@@ -24,33 +25,43 @@ public final class Body {
     public static final Measure measure;
     /** 制御 */
     public static final Control control;
+    	//---> Add 2022/06/29 T.Okado
+    // 自己位置推定クラス
+    public static final SelfPosition selfPos;
+	    //<--- Add 2022/06/29 T.Okado
 
     /** 車輪の半径(mm) */
-    public static final float RADIUS = 51.0f;
+    public static final float RADIUS = 50.0f;
     /** 車輪の直径(mm) */
     public static final float DIAMETER = 2.0f * RADIUS;
     /** 左右動輪の間隔(mm) */
     public static final float TREAD = 147.0f;
+    //---> Add 2022/06/20 T.Okado
+    /** 車輪の円周(mm) */
+    public static final float CIRCLE = 320.0f;
+    //<--- Add 2022/06/20 T.Okado
 
     static {
 
-        // ハードウェアの初期化
-        EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
+         // ハードウェアの初期化
+		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
         EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S2);
-        KamogawaRegulatedMotor leftMotor = new KamogawaRegulatedMotor(MotorPort.B);
-        KamogawaRegulatedMotor rightMotor = new KamogawaRegulatedMotor(MotorPort.C);
-        KamogawaRegulatedMotor armMotor = new KamogawaRegulatedMotor(MotorPort.A);
+		KamogawaRegulatedMotor leftMotor = new KamogawaRegulatedMotor(MotorPort.C);
+		KamogawaRegulatedMotor rightMotor = new KamogawaRegulatedMotor(MotorPort.B);
 
-        // 計測の初期化
-        MeasureTouch measureTouch = new MeasureTouch(touchSensor);
+		// 計測の初期化
+		MeasureTouch measureTouch = new MeasureTouch(touchSensor);
         MeasureCourse measureCourse = new MeasureCourse(colorSensor);
-        MeasureWheel measureWheel = new MeasureWheel(leftMotor, rightMotor);
-        MeasureArm measureArm = new MeasureArm(armMotor);
-        measure = new Measure(measureTouch, measureCourse, measureWheel, measureArm);
+		MeasureWheel measureWheel = new MeasureWheel(leftMotor, rightMotor);
+		measure = new Measure(measureTouch, measureCourse, measureWheel);
         // 制御の初期化
-        ControlWheel controlWheel = new ControlWheel(leftMotor, rightMotor);
-        ControlArm controlArm = new ControlArm(armMotor);
-        control = new Control(controlWheel, controlArm);
+		ControlWheel controlWheel = new ControlWheel(leftMotor, rightMotor);
+		control = new Control(controlWheel);
+
+		//---> Add 2022/06/29 T.Okado
+	    // 自己位置推定クラス
+	    selfPos = new SelfPosition();
+		//<--- Add 2022/06/29 T.Okado
 
     }
 
