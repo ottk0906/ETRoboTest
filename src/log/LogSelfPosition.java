41b1a5e6457e5cac49b16320004e88233a2a4740
おかど　たけとし <106789869+t-okado@users.noreply.github.com>
2022-07-08 15:56:14 +0900

    Add files via upload

--- src/log/LogSelfPosition.java
+++ src/log/LogSelfPosition.java
@@ -80,7 +80,8 @@ public class LogSelfPosition {
         		selfPos.getRightDistnce(),
         		selfPos.getDistnce(),
         		selfPos.getLeftBeforeAngle(),
-        		selfPos.getRightBeforeAngle()
+        		selfPos.getRightBeforeAngle(),
+        		Body.stopwatch.elapsed()
         	)
     	);
     }
@@ -93,8 +94,8 @@ public class LogSelfPosition {
         LCD.drawString(String.valueOf(selfPos.getXCoord()) , 11, 5);
         LCD.drawString("Y-Coord", 0, 6);
         LCD.drawString(String.valueOf(selfPos.getYCoord()), 11, 6);
-        LCD.drawString("Angle", 0, 7);
-        LCD.drawString(String.valueOf(selfPos.getAfterRadianToAngle()), 11, 7);
+        LCD.drawString("Distance", 0, 7);
+        LCD.drawString(String.valueOf(selfPos.getAccumDistance()), 11, 7);
     }
 
     /**
@@ -117,7 +118,8 @@ public class LogSelfPosition {
 	        	sb.append("RigjtWheel-Distance").append(",");
 	        	sb.append("Distance").append(",");
 	        	sb.append("LeftWheel-Angle-Past").append(",");
-	        	sb.append("RightWheel-Angle-Past").append("\r\n");
+	        	sb.append("RightWheel-Angle-Past").append(",");
+	        	sb.append("elapsedTime").append("\r\n");
         	//データ部分を出力する
             } else {
 	        	for (LogSelfPositionData data : logList) {
